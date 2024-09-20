afaf92364417345439eed6d2fa1a383b3bac64c5
Kahsolt <kahsolt@qq.com>
2024-09-20 14:11:42 +0800

    add contest demo project

--- repo/init_repos.cmd
+++ repo/init_repos.cmd
@@ -2,6 +2,11 @@
 
 PUSHD %~dp0
 
+REM contest demo project
+wget -nc https://competition-main.oss-cn-beijing.aliyuncs.com/dfadminwebsite-production/uploads/images/competitions/1044/ocr-595521.zip
+wget -nc https://competition-main.oss-cn-beijing.aliyuncs.com/dfadminwebsite-production/uploads/images/competitions/1044/datasets-101982.zip
+
+REM chineseocr_lite
 git clone https://github.com/DayBreak-u/chineseocr_lite
 
 POPD
